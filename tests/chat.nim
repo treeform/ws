cbb8f763b436669392d10baec2a45778395395cc
treeform <starplant@gmail.com>
2025-09-19 20:17:40 -0700

    FIx {.gcsafe.} issue in test.

--- tests/chat.nim
+++ tests/chat.nim
@@ -6,14 +6,16 @@ proc cb(req: Request) {.async, gcsafe.} =
   if req.url.path == "/ws":
     try:
       var ws = await newWebSocket(req)
-      connections.add ws
+      {.gcsafe.}:
+        connections.add ws
       await ws.send("Welcome to simple chat server")
       while ws.readyState == Open:
         let packet = await ws.receiveStrPacket()
         echo "Received packet: " & packet
-        for other in connections:
-          if other.readyState == Open:
-            asyncCheck other.send(packet)
+        {.gcsafe.}:
+          for other in connections:
+            if other.readyState == Open:
+              asyncCheck other.send(packet)
     except WebSocketClosedError:
       echo "Socket closed. "
     except WebSocketProtocolMismatchError:
